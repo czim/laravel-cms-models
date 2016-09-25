e38571451c17559dee27cee08915b142173865e6
Coen Zimmerman <coen@pixelindustries.com>
2016-09-25 21:48:28 +0200

    Listing tweaks

--- resources/views/model/index.blade.php
+++ resources/views/model/index.blade.php
@@ -204,7 +204,7 @@
 
 
     <div id="delete-record-modal" class="modal fade" tabindex="-1" role="dialog">
-        <div class="modal-dialog" role="document">
+        <div class="modal-dialog modal-sm" role="document">
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-label="{{ ucfirst(cms_trans('common.action.close')) }}">
@@ -378,7 +378,7 @@
                 // set the current value on the input field
                 input.val( parent.attr('data-position') );
 
-                setTimeout(function() { input.focus(); }, 500);
+                setTimeout(function() { input.focus().select(); }, 500);
 
                 // store the index of the table row on the modal
                 $('#orderable-position-modal').data('data-tr-index', parent.closest('tr').index());
