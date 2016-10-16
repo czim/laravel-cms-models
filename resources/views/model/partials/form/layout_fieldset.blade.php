5d43e2f35d564609c235240cf6c549760ba3074f
Coen Zimmerman <coen@pixelindustries.com>
2016-10-16 14:02:49 +0200

    Added legend to Form fieldset partial

--- resources/views/model/partials/form/layout_fieldset.blade.php
+++ resources/views/model/partials/form/layout_fieldset.blade.php
@@ -1,6 +1,8 @@
 
 <fieldset id="fieldset-{{ $key }}">
 
+    <legend>{{ $fieldset->display() }}</legend>
+
     @foreach ($fieldset->children as $nodeKey => $node)
 
         @include('cms-models::model.partials.form.layout_node', array_merge(
