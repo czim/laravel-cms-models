dc6859157a1c9abf3d6b1e64354e094686ba5034
Coen Zimmerman <coen@pixelindustries.com>
2016-10-17 19:27:15 +0200

    Form validation rule enrichment updated: allow for selective key addition

--- src/Repositories/Collectors/Enricher/EnrichValidationData.php
+++ src/Repositories/Collectors/Enricher/EnrichValidationData.php
@@ -10,6 +10,14 @@ class EnrichValidationData extends AbstractEnricherStep
 {
     use ResolvesFormStoreStrategies;
 
+
+    /**
+     * Originally configured create rules.
+     *
+     * @var array
+     */
+    protected $originalCreateRules;
+
     /**
      * Performs enrichment of validation rules based on form field strategies.
      */
@@ -19,9 +27,107 @@ class EnrichValidationData extends AbstractEnricherStep
             return;
         }
 
+        $this->enrichCreateRules()
+             ->enrichUpdateRules();
+    }
+
+    /**
+     * Enriches validation rules for (default or) create form data.
+     *
+     * @return $this
+     */
+    protected function enrichCreateRules()
+    {
         $rules = $this->info->validation['create'] ?: [];
 
+        // Store original rules so they may be used as a basis for update rules later.
+        $this->originalCreateRules = $rules;
+
+        $formRules = $this->getFormFieldBaseRules(true);
+
+        if ( ! count($rules)) {
+            $rules = $formRules;
+        } else {
+            $rules = $this->enrichRulesWithFormRules($rules, $formRules);
+        }
+
+        $this->info->validation['create'] = $rules;
+
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    protected function enrichUpdateRules()
+    {
+        $rules = $this->info->validation['update'];
+
+        $formRules = $this->getFormFieldBaseRules(false);
+
+        // If no specific update rules were defined, use the
+        // create rules as a starting point.
+
+        if (null === $rules) {
+            $rules = $this->originalCreateRules;
+        }
+
+        if ( ! count($rules)) {
+            $rules = $formRules;
+        } else {
+            $rules = $this->enrichRulesWithFormRules($rules, $formRules);
+        }
+
+        $this->info->validation['update'] = $rules;
+
+        return $this;
+    }
+
+    /**
+     * Enrich a given set of rules with form field data determined.
+     *
+     * @param array $rules      rules to be enriched
+     * @param array $formRules  rules determined based on form field data
+     * @return array
+     */
+    protected function enrichRulesWithFormRules(array $rules, array $formRules)
+    {
+        // If rules are set, they should not overwritten,
+        // and unkeyed string values should be enriched.
+        // But keys not present should not be enriched or included.
+
+        $enrichedRules = [];
+
+        foreach ($rules as $key => $ruleParts) {
+
+            if (    ! is_string($ruleParts) || ! is_numeric($key)
+                ||  ! array_key_exists($ruleParts, $formRules)
+            ) {
+                continue;
+            }
+
+            $enrichedRules[ $ruleParts ] = $formRules[ $ruleParts ];
+        }
+
+        return $enrichedRules;
+    }
+
+    /**
+     * Returns rules determined by form field strategies.
+     *
+     * @param bool $forCreate
+     * @return array
+     */
+    protected function getFormFieldBaseRules($forCreate = true)
+    {
+        $rules = [];
+
         foreach ($this->info->form->fields as $field) {
+
+            // Leave out fields that are not relevant
+            if ($forCreate && ! $field->create() || ! $forCreate && ! $field->update()) {
+                continue;
+            }
 
             $instance = $this->getFormFieldStoreStrategyInstanceForField($field);
 
@@ -44,7 +150,7 @@ class EnrichValidationData extends AbstractEnricherStep
             }
         }
 
-        $this->info->validation['create'] = $rules;
+        return $rules;
     }
 
     /**
