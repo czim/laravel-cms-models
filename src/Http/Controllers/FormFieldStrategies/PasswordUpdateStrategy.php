f30d7ab0a0948f9e839ab6b1f640409b21401c98
Coen Zimmerman <coen@pixelindustries.com>
2016-11-20 16:51:41 +0100

    PasswordUpdate store strategy now retrieves only null

--- src/Http/Controllers/FormFieldStrategies/PasswordUpdateStrategy.php
+++ src/Http/Controllers/FormFieldStrategies/PasswordUpdateStrategy.php
@@ -7,9 +7,28 @@ use Illuminate\Database\Eloquent\Model;
  * Class PasswordUpdateStrategy
  *
  * For setting a new hashed password, but only if a value is submitted.
+ * Also returns null as value no matter what is currently set.
  */
 class PasswordUpdateStrategy extends AbstractFormFieldStoreStrategy
 {
+
+    /**
+     * {@inheritdoc}
+     */
+    public function retrieve(Model $model, $source)
+    {
+        if ($this->isTranslated()) {
+            $keys = $model->translations->pluck(config('translatable.locale_key', 'locale'))->toArray();
+
+            $nulls = [];
+            foreach ($keys as $locale) {
+                $nulls[ $locale ] = null;
+            }
+            return $nulls;
+        }
+
+        return null;
+    }
 
     /**
      * {@inheritdoc}
