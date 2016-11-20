fcbdce475480c5cb271a99b30c5fc903645cde17
Coen Zimmerman <coen@pixelindustries.com>
2016-11-20 15:20:20 +0100

    Default form field display now uses options to set input attributes

--- resources/views/model/partials/form/strategies/default.blade.php
+++ resources/views/model/partials/form/strategies/default.blade.php
@@ -5,6 +5,14 @@
        value="{{ $value }}"
        class="form-control"
        @if ($required && ! $translated) required="required" @endif
+       size="{{ array_get($options, 'size', array_get($options, 'maxlength')) }}"
+       @if (array_get($options, 'maxlength'))
+        maxlength="{{ array_get($options, 'maxlength') }}"
+       @endif
+       @if (null !== array_get($options, 'min')) min="{{ array_get($options, 'min') }}" @endif
+       @if (null !== array_get($options, 'max')) max="{{ array_get($options, 'max') }}" @endif
+       @if (array_get($options, 'step')) step="{{ array_get($options, 'step') }}" @endif
+       @if (array_get($options, 'pattern')) pattern="{{ array_get($options, 'pattern') }}" @endif
 >
 
 @include('cms-models::model.partials.form.field_errors', compact('key', 'errors'))
