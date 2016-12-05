37d7a2f4cee9225fb11ce2c1e41ab1af6475f32e
Coen Zimmerman <coen@pixelindustries.com>
2016-12-05 10:32:03 +0100

    Added ‘required’ css class to some form layout partials

--- resources/views/model/partials/form/field_strategy.blade.php
+++ resources/views/model/partials/form/field_strategy.blade.php
@@ -3,7 +3,7 @@
 <div class="form-group row @if (array_has($errors, $key)) has-error @endif">
 
     @if ( ! ($parent instanceof \Czim\CmsModels\Support\Data\ModelFormFieldGroupData))
-        <label for="field-{{ $key }}" class="control-label col-sm-2">
+        <label for="field-{{ $key }}" class="control-label col-sm-2 @if ($field->required()) required @endif">
             {{ $field->label() }}
         </label>
     @endif
