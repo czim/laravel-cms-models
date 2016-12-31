ceced9dde6594208b7ab2599803b0809e6806dd9
Coen Zimmerman <coen@pixelindustries.com>
2016-12-31 16:21:13 +0100

    Minor tweak for back to all/top link for list parent scoped results

--- resources/views/model/partials/index_parent_back_button.blade.php
+++ resources/views/model/partials/index_parent_back_button.blade.php
@@ -2,8 +2,14 @@
 <div class="btn-group">
     <a href="{{ cms_route("{$routePrefix}.index") }}?parents=" class="btn btn-default">
         <i class="fa fa-arrow-up"></i> &nbsp;
-        {{ cms_trans('models.list-parents.back-to-all-models', [
-            'models' => ucfirst($model->verbose_name_plural)
-        ]) }}
+        @if ($model->list->default_top_relation)
+            {{ cms_trans('models.list-parents.back-to-top-models', [
+                'models' => ucfirst($model->verbose_name_plural)
+            ]) }}
+        @else
+            {{ cms_trans('models.list-parents.back-to-all-models', [
+                'models' => ucfirst($model->verbose_name_plural)
+            ]) }}
+        @endif
     </a>
 </div>
