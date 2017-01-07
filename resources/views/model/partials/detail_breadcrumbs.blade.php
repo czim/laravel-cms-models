dc5028b2fe23b79c8e454a19bd4dddaecad29e13
Coen Zimmerman <coen@pixelindustries.com>
2017-01-07 12:24:32 +0100

    Fixed breadcrumb issue for list parents on create/edit page

--- resources/views/model/partials/detail_breadcrumbs.blade.php
+++ resources/views/model/partials/detail_breadcrumbs.blade.php
@@ -8,30 +8,41 @@
 
     @if ($hasActiveListParent)
 
+        <?php
+            $previousListParent = null;
+        ?>
+
         @foreach ($listParents as $listParent)
 
             <li>
                 @if (cms_auth()->can($listParent->permission_prefix . 'show'))
-                    <a href="{{ cms_route($listParent->route_prefix . '.index') }}">
-                        {{ ucfirst($listParent->information->verbose_name_plural) }}
+                    <a href="{{ cms_route($listParent->route_prefix . '.index') }}{{ null !== $listParent->query ? '?' . $listParent->query : null }}">
+                        @endif
+
+                        @if ($previousListParent)
+
+                            {{ cms_trans('models.list-parents.children-for-parent-with-id', [
+                                'children' => ucfirst($listParent->information->verbose_name_plural),
+                                'parent'   => $previousListParent->information->verbose_name,
+                                'id'       => $previousListParent->model->incrementing
+                                                ?   '#' . $previousListParent->model->getKey()
+                                                :   "'" . $previousListParent->model->getKey() . "'",
+                            ]) }}
+
+                        @else
+                            {{ ucfirst($listParent->information->verbose_name_plural) }}
+                        @endif
+
+                        @if (cms_auth()->can($listParent->permission_prefix . 'show'))
                     </a>
-                @else
-                    {{ ucfirst($listParent->information->verbose_name_plural) }}
                 @endif
             </li>
 
-        @endforeach
+            <?php
+                $previousListParent = $listParent;
+            ?>
 
-        <?php /*
-            // todo: consider whether this should be used or not
-            <li>
-                <a href="{{ cms_route("{$routePrefix}.index") }}?parent=">
-                    {{ cms_trans('models.list-parents.all-models', [
-                        'models' => ucfirst($model->verbose_name_plural)
-                    ]) }}
-                </a>
-            </li>
-        */ ?>
+        @endforeach
 
         <?php $listParent = last($listParents) ?>
 
