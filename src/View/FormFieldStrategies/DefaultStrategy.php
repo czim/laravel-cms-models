47d434a7fb7e191e57d40bc75bdba740fa066961
Coen Zimmerman <coen@pixelindustries.com>
2017-01-15 16:46:43 +0100

    Default form field strategy throws exception if value is cannot be cast to string

--- src/View/FormFieldStrategies/DefaultStrategy.php
+++ src/View/FormFieldStrategies/DefaultStrategy.php
@@ -1,6 +1,8 @@
 <?php
 namespace Czim\CmsModels\View\FormFieldStrategies;
 
+use Czim\CmsModels\Exceptions\FormFieldDisplayException;
+
 class DefaultStrategy extends AbstractDefaultStrategy
 {
 
@@ -12,6 +14,32 @@ class DefaultStrategy extends AbstractDefaultStrategy
     protected function getView()
     {
         return 'cms-models::model.partials.form.strategies.default';
+    }
+
+    /**
+     * Enriches field data before passing it on to the view.
+     *
+     * @param array $data
+     * @return array
+     * @throws FormFieldDisplayException
+     */
+    protected function decorateFieldData(array $data)
+    {
+        // Prevent causing errors in the view if the value is not castable to string.
+        try {
+            $data['value'] = (string) $data['value'];
+
+        } catch (\Exception $e) {
+
+            throw new FormFieldDisplayException(
+                "Failed to cast value to string for field '{$this->field->key()}' "
+                . "with source '{$this->field->source()}'",
+                0,
+                $e
+            );
+        }
+
+        return $data;
     }
 
 }
