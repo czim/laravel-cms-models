3e50e48214879c973c0fe241733373e5cbfef6cc
Coen Zimmerman <coen@pixelindustries.com>
2017-01-21 22:54:49 +0100

    Minor model detail page breadcrumbs fix

--- resources/views/model/partials/detail_breadcrumbs.blade.php
+++ resources/views/model/partials/detail_breadcrumbs.blade.php
@@ -2,7 +2,7 @@
 <ol class="breadcrumb">
     <li>
         <a href="{{ cms_route(\Czim\CmsCore\Support\Enums\NamedRoute::HOME) }}">
-            {{ cms_trans('common.home') }}
+            {{ ucfirst(cms_trans('common.home')) }}
         </a>
     </li>
 
@@ -17,23 +17,23 @@
             <li>
                 @if (cms_auth()->can($listParent->permission_prefix . 'show'))
                     <a href="{{ cms_route($listParent->route_prefix . '.index') }}{{ null !== $listParent->query ? '?' . $listParent->query : null }}">
-                        @endif
+                @endif
 
-                        @if ($previousListParent)
+                    @if ($previousListParent)
 
-                            {{ cms_trans('models.list-parents.children-for-parent-with-id', [
-                                'children' => ucfirst($listParent->information->verbose_name_plural),
-                                'parent'   => $previousListParent->information->verbose_name,
-                                'id'       => $previousListParent->model->incrementing
-                                                ?   '#' . $previousListParent->model->getKey()
-                                                :   "'" . $previousListParent->model->getKey() . "'",
-                            ]) }}
+                        {{ cms_trans('models.list-parents.children-for-parent-with-id', [
+                            'children' => ucfirst($listParent->information->verbose_name_plural),
+                            'parent'   => $previousListParent->information->verbose_name,
+                            'id'       => $previousListParent->model->incrementing
+                                            ?   '#' . $previousListParent->model->getKey()
+                                            :   "'" . $previousListParent->model->getKey() . "'",
+                        ]) }}
 
-                        @else
-                            {{ ucfirst($listParent->information->verbose_name_plural) }}
-                        @endif
+                    @else
+                        {{ ucfirst($listParent->information->verbose_name_plural) }}
+                    @endif
 
-                        @if (cms_auth()->can($listParent->permission_prefix . 'show'))
+                @if (cms_auth()->can($listParent->permission_prefix . 'show'))
                     </a>
                 @endif
             </li>
