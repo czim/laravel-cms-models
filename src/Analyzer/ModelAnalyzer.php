38e56612e8a76c3fa48e19a5586099e3e6b460d8
Coen Zimmerman <coen@pixelindustries.com>
2017-02-27 13:06:57 +0100

    Added safeguard to model analyzer to prevent including unusable scopes

--- src/Analyzer/ModelAnalyzer.php
+++ src/Analyzer/ModelAnalyzer.php
@@ -399,23 +399,13 @@ class ModelAnalyzer
 
         foreach ($this->reflection->getMethods() as $method) {
 
-            if ( ! starts_with($method->name, 'scope')) {
+            if ( ! starts_with($method->name, 'scope') || ! $this->isScopeMethodUsable($method)) {
                 continue;
             }
 
             $scopeName = camel_case(substr($method->name, 5));
 
-            $cmsTags = $this->getCmsDocBlockTags($method);
-
-            if (    array_get($cmsTags, 'ignore')
-                ||  (   ! array_get($cmsTags, 'scope')
-                    &&  in_array($scopeName, $this->getIgnoredScopeNames() )
-                )
-            ) {
-                continue;
-            }
-
-            // store the scope name without the scope prefix
+            // Store the scope name without the scope prefix
             $scopes[ $scopeName ] = new ModelScopeData([
                 'method'   => $scopeName,
                 'label'    => null,
@@ -426,6 +416,40 @@ class ModelAnalyzer
         $this->info->list->scopes = $scopes;
 
         return $this;
+    }
+
+    /**
+     * Returns whether a reflection method is usabled as a CMS scope.
+     *
+     * @param ReflectionMethod $method
+     * @return bool
+     */
+    protected function isScopeMethodUsable(ReflectionMethod $method)
+    {
+        // Scope methods with more or less required parameters than the query, should not be used.
+        if ($method->getNumberOfRequiredParameters() != 1) {
+            return false;
+        }
+
+        // If the required parameter is not the first, don't use it
+        if ( ! ($firstParameter = head($method->getParameters())) || $firstParameter->isOptional()) {
+            return false;
+        }
+
+        $scopeName = camel_case(substr($method->name, 5));
+
+        // Ignore scope methods with an explicity ignore cms docblock tag
+        $cmsTags = $this->getCmsDocBlockTags($method);
+
+        if (    array_get($cmsTags, 'ignore')
+            ||  (   ! array_get($cmsTags, 'scope')
+                &&  in_array($scopeName, $this->getIgnoredScopeNames() )
+            )
+        ) {
+            return false;
+        }
+
+        return true;
     }
 
     /**
