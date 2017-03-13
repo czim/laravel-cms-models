de5c67080287353d9c039b9f421e2a6db6c29811
Coen Zimmerman <coen@pixelindustries.com>
2017-03-13 20:49:23 +0100

    Added some tests for model analyzer steps

---
+++ tests/Analyzer/Processor/Steps/SetBasicInformationTest.php
@@ -0,0 +1,35 @@
+<?php
+namespace Czim\CmsModels\Test\Analyzer\Processor\Steps;
+
+use Czim\CmsModels\Analyzer\Processor\Steps\SetBasicInformation;
+use Czim\CmsModels\Support\Data\ModelInformation;
+use Czim\CmsModels\Test\Helpers\Models\TestPost;
+
+class SetBasicInformationTest extends AbstractStepCase
+{
+
+    /**
+     * @test
+     */
+    function it_sets_information_based_on_the_model()
+    {
+        // Setup
+        $model    = new TestPost;
+        $analyzer = $this->prepareAnalyzerSetup($model);
+        $info     = new ModelInformation;
+
+        // Test
+        $step = new SetBasicInformation;
+        $step->setAnalyzer($analyzer);
+
+        $info = $step->analyze($info);
+
+        static::assertEquals('test post', $info['verbose_name']);
+        static::assertEquals('test posts', $info['verbose_name_plural']);
+        static::assertEquals('models.name.test post', $info['translated_name']);
+        static::assertEquals('models.name.test posts', $info['translated_name_plural']);
+
+        static::assertTrue($info['incrementing']);
+    }
+
+}
