b7518e3eda9b7b893dc49eeeba517ee1d55ff218
Coen Zimmerman <coen@pixelindustries.com>
2017-03-29 14:07:01 +0200

    Fixed formrequests forcing JSON when not explicitly accepted by request

--- src/Http/Requests/Request.php
+++ src/Http/Requests/Request.php
@@ -2,7 +2,25 @@
 namespace Czim\CmsModels\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\JsonResponse;
 
 abstract class Request extends FormRequest
 {
+
+    /**
+     * Overridden to only return JSON response if explicitly requested.
+     *
+     * {@inheritdoc}
+     */
+    public function response(array $errors)
+    {
+        if ($this->wantsJson()) {
+            return new JsonResponse($errors, 422);
+        }
+
+        return $this->redirector->to($this->getRedirectUrl())
+            ->withInput($this->except($this->dontFlash))
+            ->withErrors($errors, $this->errorBag);
+    }
+
 }
