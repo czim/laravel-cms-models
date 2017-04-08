f8067753268a37b3aedc337c6fd5878af7c9305c
Coen Zimmerman <coen@pixelindustries.com>
2017-04-08 18:53:55 +0200

    Tiny cleanup for children action strategy

--- src/Strategies/Action/ChildrenStrategy.php
+++ src/Strategies/Action/ChildrenStrategy.php
@@ -6,6 +6,7 @@ use Czim\CmsModels\Contracts\Routing\RouteHelperInterface;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Database\Eloquent\Relations\Relation;
+use UnexpectedValueException;
 
 class ChildrenStrategy extends AbstractActionStrategy
 {
@@ -114,7 +115,7 @@ class ChildrenStrategy extends AbstractActionStrategy
         $this->relation = $this->getRelationName();
 
         if ( ! $this->relation) {
-            throw new \UnexpectedValueException(
+            throw new UnexpectedValueException(
                 "Could not determine relation for children link action for model {$this->otherModelClass}"
             );
         }
@@ -130,7 +131,7 @@ class ChildrenStrategy extends AbstractActionStrategy
         $relationInstance = (new $this->otherModelClass)->{$this->relation}();
 
         if ( ! ($relationInstance instanceof Relation)) {
-            throw new \UnexpectedValueException(
+            throw new UnexpectedValueException(
                 "Method '{$this->relation}' on model '{$this->otherModelClass}' is not a relation"
             );
         }
@@ -152,7 +153,7 @@ class ChildrenStrategy extends AbstractActionStrategy
         $this->otherModelClass = array_get($this->actionData->options(), 'model');
 
         if ( ! $this->otherModelClass || ! is_a($this->otherModelClass, Model::class, true)) {
-            throw new \UnexpectedValueException("{$this->otherModelClass} is not a valid target model class");
+            throw new UnexpectedValueException("{$this->otherModelClass} is not a valid target model class");
         }
 
         /** @var ModelInformationRepositoryInterface $infoRepository */
@@ -161,7 +162,7 @@ class ChildrenStrategy extends AbstractActionStrategy
         $info = $infoRepository->getByModelClass($this->otherModelClass);
 
         if ( ! $info) {
-            throw new \UnexpectedValueException("{$this->otherModelClass} is not a CMS model");
+            throw new UnexpectedValueException("{$this->otherModelClass} is not a CMS model");
         }
 
         return $this->otherModelClass;
