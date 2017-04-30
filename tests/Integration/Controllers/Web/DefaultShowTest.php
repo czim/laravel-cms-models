75c2fe9f65d07fac1de3d9edd2c86919b4659c47
Coen Zimmerman <tabtemp@xs4all.nl>
2017-04-30 10:10:14 +0200

    Added simple show page integration test

---
+++ tests/Integration/Controllers/Web/DefaultShowTest.php
@@ -0,0 +1,36 @@
+<?php
+namespace Czim\CmsModels\Test\Integration\Controllers\Web;
+
+use Czim\CmsModels\Test\Integration\Controllers\AbstractControllerIntegrationTest;
+
+/**
+ * Class DefaultShow
+ *
+ * Tests for simple model show page. Note that browser/javascript functionality is deliberately not tested here.
+ *
+ * @group integration
+ * @group controllers
+ */
+class DefaultShow extends AbstractControllerIntegrationTest
+{
+    const ROUTE_BASE = 'cms::models.model.czim-cmsmodels-test-helpers-models-testpost';
+
+
+    /**
+     * @test
+     */
+    function it_shows_model_data()
+    {
+        $this->visitRoute(static::ROUTE_BASE . '.show', [1])->seeStatusCode(200);
+
+        $groups = $this->crawler()->filter('div.form-group.row');
+
+        static::assertCount(9, $groups, 'Number of fields shown does not match');
+
+        $group = $groups->first();
+
+        static::assertEquals('Id', trim($group->filter('label')->html()), 'Field label for ID mismatch');
+        static::assertEquals('1', trim($group->filter('div div')->html()), 'Field value for ID mismatch');
+    }
+
+}
