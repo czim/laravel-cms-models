326a23fa18cfc7d6a51889ee882d0edb22c9f6df
Coen Zimmerman <tabtemp@xs4all.nl>
2017-05-15 16:54:02 +0200

    Minor cleanup: replaced hardcoded locale placeholder with constant

--- src/Strategies/Form/Store/AbstractFormFieldStoreStrategy.php
+++ src/Strategies/Form/Store/AbstractFormFieldStoreStrategy.php
@@ -264,7 +264,7 @@ class AbstractFormFieldStoreStrategy implements FormFieldStoreStrategyInterface
 
         $translated = array_map(
             function ($key) {
-                return $key . '.<trans>';
+                return $key . '.' . TranslationLocaleHelper::VALIDATION_LOCALE_PLACEHOLDER;
             },
             $translated
         );
