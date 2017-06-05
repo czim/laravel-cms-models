b3ff0b9f25440e2049920ecfafd9912cd3f3b918
Coen Zimmerman <tabun@xs4all.nl>
2017-06-05 18:26:36 +0200

    Added standard label & field help text & partial

--- resources/views/model/partials/form/field_strategy.blade.php
+++ resources/views/model/partials/form/field_strategy.blade.php
@@ -3,7 +3,22 @@
     <div class="form-group row @if (array_has($errors, $key)) has-error @endif">
 
         <label for="field-{{ $key }}" class="control-label col-sm-2 @if ($field->required()) required @endif">
-            {{ $field->label() }}
+            <span class="field-label">
+                {{ $field->label() }}
+            </span>
+
+            {{-- Help text --}}
+            @if ($field->help->label)
+                @php
+                    $partial = $field->help->label->view() ?: 'cms-models::model.partials.form.field_help';
+                @endphp
+                @include($partial, [
+                    'id'     => 'field-label-helptext-' . $key,
+                    'text'   => $field->help->label->text(),
+                    'class'  => $field->help->label->cssClass(),
+                    'escape' => $field->help->label->escape(),
+                ])
+            @endif
         </label>
 @endif
 
@@ -15,6 +30,19 @@
         @endif
 
         {!! $strategy !!}
+
+        {{-- Help text --}}
+        @if ($field->help->field)
+            @php
+                $partial = $field->help->field->view() ?: 'cms-models::model.partials.form.field_help';
+            @endphp
+            @include($partial, [
+                'id'     => 'field-helptext-' . $key,
+                'text'   => $field->help->field->text(),
+                'class'  => $field->help->field->cssClass(),
+                'escape' => $field->help->field->escape(),
+            ])
+        @endif
 
         {{-- After view --}}
         @if ($field->after && $field->after->view)
