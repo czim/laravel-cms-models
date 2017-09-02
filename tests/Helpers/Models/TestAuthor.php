5538e32b4be64d4dbe2e645a06f2a74e60ddc1e9
Coen Zimmerman <tabun@xs4all.nl>
2017-09-02 23:46:37 +0200

    Added temporary fix to allow testing with Stapler in Laravel 5.5

--- tests/Helpers/Models/TestAuthor.php
+++ tests/Helpers/Models/TestAuthor.php
@@ -1,14 +1,14 @@
 <?php
 namespace Czim\CmsModels\Test\Helpers\Models;
 
-use Codesleeve\Stapler\ORM\EloquentTrait;
 use Codesleeve\Stapler\ORM\StaplerableInterface;
+use Czim\CmsModels\Test\Helpers\Models\Stapler\EloquentTraitFixed;
 use Czim\Listify\Listify;
 use Illuminate\Database\Eloquent\Model;
 
 class TestAuthor extends Model implements StaplerableInterface
 {
-    use EloquentTrait,
+    use EloquentTraitFixed,
         Listify;
 
     protected $fillable = [
