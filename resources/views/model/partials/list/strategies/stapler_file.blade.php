510b5b1dc40045277e28512193f3de2bf95174c8
Coen Zimmerman <tabun@xs4all.nl>
2018-02-13 20:47:40 +0100

    Removed unused staplerclip strategy files

--- resources/views/model/partials/list/strategies/stapler_file.blade.php
+++
@@ -1,6 +0,0 @@
-@if ($exists)
-    <a class="stapler-file" href="{{ $url }}" target="_blank">
-        {{ $filename }}
-    </a>
-@endif
-
--- resources/views/model/partials/list/strategies/stapler_image.blade.php
+++
@@ -1,6 +0,0 @@
-@if ($exists)
-    <img class="stapler-thumbnail" src="{{ $urlThumb }}" alt="{{ $filename }}" height="{{ $height }}">
-@else
-    <div class="missing-image" style="@if ($width) width: {{ $width }}px;@endif @if ($height) height: {{ $height }}px @endif"></div>
-@endif
-
