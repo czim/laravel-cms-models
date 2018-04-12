b71ddf778180ba3d0134d68943a2209ea7d43282
Daniel de Wit <daniel@danieldewit.nl>
2018-04-12 17:24:39 +0200

    Unchecked checkbox not included in formdata

--- resources/views/model/partials/form/strategies/boolean_checkbox.blade.php
+++ resources/views/model/partials/form/strategies/boolean_checkbox.blade.php
@@ -1,4 +1,7 @@
-
+<input type="hidden"
+       name="{{ $name ?: (isset($baseKey) ? $baseKey : $key) }}"
+       value="0"
+>
 <input id="field-{{ $key }}"
        type="checkbox"
        name="{{ $name ?: (isset($baseKey) ? $baseKey : $key) }}"
