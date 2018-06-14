bfa76d26e821881b4a10a9e08b5cc21b040306fd
Coen Zimmerman <tabun@xs4all.nl>
2018-06-14 20:58:17 +0200

    Added validation rule merger

--- src/Contracts/Support/Validation/ValidationRuleMergerInterface.php
+++ src/Contracts/Support/Validation/ValidationRuleMergerInterface.php
@@ -1,18 +1,27 @@
 <?php
 namespace Czim\CmsModels\Contracts\Support\Validation;
 
-use Czim\CmsModels\Contracts\ModelInformation\Data\Form\Validation\ValidationRuleCollectionInterface;
 use Czim\CmsModels\Contracts\ModelInformation\Data\Form\Validation\ValidationRuleDataInterface;
 
 interface ValidationRuleMergerInterface
 {
 
     /**
+     * Takes any duplicate presence of a key() in a set of rules and
+     * collapses the rules into a single entry per key.
+     *
+     * @param ValidationRuleDataInterface[] $rules
+     * @return ValidationRuleDataInterface[]
+     */
+    public function collapseRulesForDuplicateKeys(array $rules);
+
+    /**
      * Merges together the validation rules defined for a strategy with rules defined
      * for a form field's attribute (or relation).
-     * @param array|ValidationRuleDataInterface[]|string[] $strategyRules
-     * @param array|ValidationRuleDataInterface[]|string[] $attributeRules
-     * @return ValidationRuleCollectionInterface|ValidationRuleDataInterface[]
+     *
+     * @param ValidationRuleDataInterface[] $strategyRules
+     * @param ValidationRuleDataInterface[] $attributeRules
+     * @return ValidationRuleDataInterface[]
      */
     public function mergeStrategyAndAttributeBased(array $strategyRules, array $attributeRules);
 
