ffaad6cbdb778ac457a65c5044a57d69444b1bba
Coen Zimmerman <tabun@xs4all.nl>
2018-06-14 20:58:17 +0200

    Added validation rule merger

--- src/Support/Validation/ValidationRuleMerger.php
+++ src/Support/Validation/ValidationRuleMerger.php
@@ -1,11 +1,9 @@
 <?php
 namespace Czim\CmsModels\Support\Validation;
 
-use Czim\CmsModels\Contracts\ModelInformation\Data\Form\Validation\ValidationRuleCollectionInterface;
 use Czim\CmsModels\Contracts\ModelInformation\Data\Form\Validation\ValidationRuleDataInterface;
 use Czim\CmsModels\Contracts\Support\Validation\ValidationRuleMergerInterface;
-use Czim\CmsModels\ModelInformation\Data\Form\Validation\ValidationRuleCollection;
-use Czim\CmsModels\ModelInformation\Data\Form\Validation\ValidationRuleData;
+use Illuminate\Support\Collection;
 
 /**
  * Class ValidationRuleMerger
@@ -17,116 +15,149 @@ class ValidationRuleMerger implements ValidationRuleMergerInterface
 {
 
     /**
+     * Takes any duplicate presence of a key() in a set of rules and
+     * collapses the rules into a single entry per key.
+     *
+     * @param ValidationRuleDataInterface[] $rules
+     * @return ValidationRuleDataInterface[]
+     */
+    public function collapseRulesForDuplicateKeys(array $rules)
+    {
+        $collection = (new Collection($rules))
+            // Group by key so we can process each key separately
+            ->groupBy(function (ValidationRuleDataInterface $rule) {
+                return $rule->key() ?: '';
+            })
+            // Collapse by merging separate rules into the first data item in the list, per key.
+            ->transform(function (Collection $rules) {
+                /** @var ValidationRuleDataInterface $data */
+                /** @var Collection|ValidationRuleDataInterface[] $rules */
+                $data = $rules->shift();
+
+                foreach ($rules as $rule) {
+                    $data->setRules(array_merge($data->rules(), $rule->rules()));
+                }
+
+                return $data;
+            });
+
+        $array = [];
+
+        foreach ($collection as $rule) {
+            $array[] = $rule;
+        }
+
+        return $array;
+    }
+
+    /**
      * Merges together the validation rules defined for a strategy with rules defined
      * for a form field's attribute (or relation).
      *
-     * @param array|ValidationRuleDataInterface[]|string[] $strategyRules
-     * @param array|ValidationRuleDataInterface[]|string[] $attributeRules
-     * @return ValidationRuleCollectionInterface|ValidationRuleDataInterface[]
+     * Before passing arrays of rule objects to this class, they must be normalized
+     * and collapsed per key.
+     *
+     * @param ValidationRuleDataInterface[] $strategyRules
+     * @param ValidationRuleDataInterface[] $attributeRules
+     * @return ValidationRuleDataInterface[]
      */
     public function mergeStrategyAndAttributeBased(array $strategyRules, array $attributeRules)
     {
-        $mergedRules = $this->mergeValidationRules($strategyRules, $attributeRules);
-
-        $collection = new ValidationRuleCollection;
-
-        foreach ($mergedRules as $key => $ruleData) {
-
-            // If the date is already cast as a rule data object,
-            // the key is embedded and we can ignore the index.
-            if ($ruleData instanceof ValidationRuleDataInterface) {
-                $collection->push($ruleData);
-                continue;
-            }
-
-            if ( ! is_array($ruleData)) {
-                $ruleData = [ $ruleData ];
-            }
-
-            // We can cast the rules into a rule data object, but since we do not know
-            // the full key or translation status for the field, only partial data is set.
-
-            // Since the data itself does not hold the key, the index is expected to be the
-            // partial key, as children of the as yet unsupplied field key. Decoration
-            // with the field key must prefix this before the key set here.
-
-            // However, if the provided array is non-associative, all the provided
-            // rules are for the field class -- and in that case, no keys should be
-            // set here (since all rules should be decorated with ONLY the field key later).
-            $postFixKey = is_numeric($key) ? null : $key;
-
-            $collection->push(
-                new ValidationRuleData($ruleData, $postFixKey)
-            );
+        if (empty($strategyRules)) {
+            return $attributeRules;
         }
 
-        return $collection;
-    }
-
-
-    /**
-     * Sensibly merges validation rules set specifically for a strategy and those
-     * determined by model information.
-     *
-     * @todo this needs to be able to safely deal with validationruledata instances...
-     *
-     * @param array|string|false $specificRules
-     * @param array|string|false $modelRules
-     * @return array|string|false
-     */
-    protected function mergeValidationRules($specificRules, $modelRules)
-    {
-        if (empty($specificRules)) {
-            return $modelRules;
+        if (empty($attributeRules)) {
+            return $strategyRules;
         }
-
-        if (empty($modelRules)) {
-            return $specificRules;
-        }
-
-        $specificRules = is_array($specificRules) ? $specificRules : [ $specificRules ];
-        $modelRules    = is_array($modelRules)    ? $modelRules    : [ $modelRules ];
-
-        $flippedInheritable = array_flip($this->inheritableRules());
 
         // Detect if any of the specific rules are nested, in which case the normal merging process should be skipped.
         // Though it is technically possible that these nested properties will match an attribute directly,
         // this should not be assumed -- configure validation rules manually for the best results.
 
-        // The rules are nested if they are an associative array,
-        // or validationruledata instances with (any) nonempty keys.
+        if ($this->rulesArrayContainsAnyNestedRules($strategyRules)) {
+            return $strategyRules;
+        }
+
         // todo
+        return $strategyRules;
 
-        if (count(array_filter($specificRules, 'is_array'))) {
-            return $specificRules;
-        }
 
-        // Remove rules that may not be inherited, because present in specific rules
-        array_forget($flippedInheritable, array_map([ $this, 'getRuleType' ], $specificRules));
+        // Todo: This needs to be cleaned up and ready to deal with much more complicated
+        //       scenario's than will currently occur. Right now the following are not
+        //       taken into account:
+        //          - localeIndex, translated, and requiredWith translated differences
 
-        foreach ($modelRules as $modelRule) {
+        $groupedStrategyRules = (new Collection($strategyRules))
+            ->groupBy(function (ValidationRuleDataInterface $rule) {
+                return $rule->key() ?: '';
+            });
 
-            $ruleType = $this->getRuleType($modelRule);
+        $groupedAttributeRules = (new Collection($attributeRules))
+            ->groupBy(function (ValidationRuleDataInterface $rule) {
+                return $rule->key() ?: '';
+            });
 
-            if ( ! array_key_exists($ruleType, $flippedInheritable)) {
-                continue;
+        // Make a list of inheritable rules that do not appear in the strategy rules.
+        // See if those rules appear in any of the attribute rules.
+        // If they do, add those rules (but only those specifically) to the strategy rules.
+
+        //foreach ($groupedStrategyRules as $key => $strategyRules) {
+        //
+        //    $combinedRules = $strategyRules;
+        //
+        //    // Remove rules that may not be inherited, because present in specific rules.
+        //    $flippedInheritable = array_flip($this->inheritableRules());
+        //
+        //    // List inheritable rules that do not appear in the strategy-specific rules.
+        //    array_forget($flippedInheritable, array_map([$this, 'getRuleType'], $combinedRules));
+        //
+        //
+        //}
+
+        //$combinedRules = $strategyRules;
+        //
+        //foreach ($attributeRules as $rule) {
+        //
+        //    $ruleType = $this->getRuleType($rule);
+        //
+        //    if ( ! array_key_exists($ruleType, $flippedInheritable)) {
+        //        continue;
+        //    }
+        //
+        //    $combinedRules[] = $rule;
+        //}
+        //
+        //return $combinedRules;
+    }
+
+    /**
+     * Validation rule data instances with (any) nonempty keys have nested rules.
+     *
+     * @param ValidationRuleDataInterface[] $rules
+     * @return bool
+     */
+    protected function rulesArrayContainsAnyNestedRules(array $rules)
+    {
+        foreach ($rules as $rule) {
+
+            if ( ! empty($rule->key())) {
+                return true;
             }
-
-            $specificRules[] = $modelRule;
         }
 
-        return $specificRules;
+        return false;
     }
 
     /**
      * Returns a rule type for a given validation rule.
      *
-     * @param $rule
+     * @param ValidationRuleDataInterface $rule
      * @return string
      */
-    protected function getRuleType($rule)
+    protected function getRuleType(ValidationRuleDataInterface $rule)
     {
-        if (false === ($pos = strpos($rule, ':'))) {
+        if (false === ($pos = strpos($rule->rules(), ':'))) {
             return $rule;
         }
 
